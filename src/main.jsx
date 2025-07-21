e9f0aae62098ca718b10a67bc79bef7c581b7cad
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-07-22 04:26:59 +0800

    FIX: fixed wrong package naming & page title

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Explore from "./components/Explore";
 import Projects from "./components/Projects";
