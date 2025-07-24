a364784a44392cdbe26dc5d3c539d944a00ae49a
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-07-25 03:42:53 +0800

    FEAT: added profile-picture

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,10 @@ function Layout() {
   return (
     <div className="layout-container">
       <nav className="sidebar">
+        <div className="profile">
+          <img src="src\assets\profile-picture.jpg" alt="profile picture" />
+          <p>Christopher Bertrand</p>
+        </div>
         <Link to="/">Explore</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/contacts">Contacts</Link>
