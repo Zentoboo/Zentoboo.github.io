ce25203bba92599b235c27562be60980b875dca7
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-08-29 19:16:09 +0800

    FEAT: provide proper header for the tictactoe visualization page

--- src/components/content/TicTacToePage.jsx
+++ src/components/content/TicTacToePage.jsx
@@ -10,13 +10,19 @@ export default function TicTacToePage() {
         <div className="tictactoe-page">
             {/* Left: Game */}
             <div className="game-container">
-                <h2 className="tree-title">TicTacToe</h2>
+                <div className="header">
+                    <h2 className="tree-title">TicTacToe</h2>
+                    <p className="note">play against the minimax algo</p>
+                </div>
                 <TicTacToeWithTrace onTreeUpdate={setMinimaxTree} />
             </div>
 
             {/* Right: Tree Visualizer */}
             <div className="tree-container">
-                <h2 className="tree-title">Minimax Visualizer</h2>
+                <div className="header">
+                    <h2 className="tree-title">Minimax Visualizer</h2>
+                    <p className="note">go through the visualizer manually by expanding & collapsing nodes</p>
+                </div>
                 {minimaxTree ? (
                     <TreeVisualizer node={minimaxTree} />
                 ) : (
